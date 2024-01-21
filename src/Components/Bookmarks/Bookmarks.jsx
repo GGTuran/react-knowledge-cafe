8be19f9eb594645251423c6ca819564bdc3cbb3d
GGTuran <tayyabturan67@gmail.com>
2024-01-21 18:33:40 +0600

    gg

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -3,9 +3,9 @@ import Bookmark from "../Bookmark/Bookmark";
 
 const Bookmarks = ({bookmarks}) => {
     return (
-        <div className="md:w-1/3 ml-5 "  > 
+        <div className="md:w-1/3 p-8 m-5 "  > 
 
-            <h1>Bookmarks:{bookmarks.length} </h1>
+            <h1>Bookmarked blogs:{bookmarks.length} </h1>
             {
                 bookmarks.map(bookmark => <Bookmark 
                 key={bookmark.id}
