2fc09dbb97fa64a6aea75036fc7c3b0581204aed
GGTuran <tayyabturan67@gmail.com>
2024-01-21 16:31:59 +0600

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
+import Header from './Components/Header/Header'
+
 
 function App() {
  
 
   return (
     <>
-     
-      <h1>Vite + React</h1>
+     <Header></Header>
+     <div className='md:flex'>
+      <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+     </div>
+      
      
      
     </>
